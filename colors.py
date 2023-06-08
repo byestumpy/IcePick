1e63757fddb7869211cbada34fe1afc33d22d33e
byestumpy <69059018+byestumpy@users.noreply.github.com>
2023-06-08 10:13:18 -0700

    Create colors.py

---
+++ colors.py
@@ -0,0 +1,19 @@
+black = "\033[0;30m"
+red = "\033[0;31m"
+bred = "\033[1;31m"
+green = "\033[0;32m"
+bgreen = "\033[1;32m"
+yellow = "\033[0;33m"
+byellow = "\033[1;33m"
+blue = "\033[0;34m"
+bblue = "\033[1;34m"
+purple = "\033[0;35m"
+bpurple = "\033[1;35m"
+cyan = "\033[0;36m"
+bcyan = "\033[1;36m"
+white = "\033[0;37m"
+nc = "\033[00m"
+
+num_ico_l = yellow + "[" + nc
+num_ico_r = yellow + "] " + nc
+icon = yellow + "[" + nc + nc + "+" + nc + yellow + "] " + nc
